bbcf2bc5ba87033b536ef11e3467add449c74926
Steven Taylor <steven.taylor@me.com>
2016-11-09 16:26:50 -0800

    api change

--- hikvision.js
+++ hikvision.js
@@ -30,11 +30,11 @@ function hikvision(config) {
 
 
     deviceCache.on( 'set', ( key, value ) => {
-        pub.publish('hikvision.device.insert',  JSON.stringify( { id : key, value : value } ) );
+        pub.publish('device.insert',  JSON.stringify( { module: 'hikvision', id : key, value : value } ) );
     });
 
     statusCache.on( 'set', ( key, value ) => {
-        pub.publish('hikvision.device.update',  JSON.stringify( { id : key, value : value } ) );
+        pub.publish('device.update',  JSON.stringify( { module: 'hikvision', id : key, value : value } ) );
     });
 
     function call(method, body, url) {
