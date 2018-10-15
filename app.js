dfbba3d99d339a00a7a6d50b1601bcddf3d78867
Steven Taylor <steven.taylor@me.com>
2018-10-14 18:42:56 -0700

    update

--- app.js
+++ app.js
@@ -57,9 +57,6 @@ consul.kv.get(`config/sentinel/${moduleName}`, function(err, result) {
     global.config = config;
     global.config.save();
 
-    if (global.config.newrelic) {
-        require('newrelic');
-    }
 
     let pub = redis.createClient(
         {
